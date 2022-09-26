b25337cf13ceccf86d499c4a20083ffaa1a058a5
rafed-99 <73440128+rafed-99@users.noreply.github.com>
2022-09-26 18:58:02 +0100

    crud without conn

--- src/Controllers/CatController.js
+++ src/Controllers/CatController.js
@@ -10,4 +10,52 @@ const GetCategories =(req, res) => {
     });
 };
 
+const GetCategoriesById = (req,res) => {
+    db.query("Select * from category WHERE idCategory = ?" ,[req.params.idCategory] ,(err, row) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }  
+    });
+};
+
+const AddCategory = (req,res) => {
+
+    const params = req.body;
+    db.query("Insert into category set ?" ,params, (err, row) =>{
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+};
+
+const DeleteCategory =(req,res) => {
+
+    db.query("Delete from category where idCategory = ?" ,[req.params.idCategory],(err,row) =>{
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+};
+
+const UpdateCategory =(req,res) => {
+
+    db.query("Delete from category where idCategory = ?" ,[req.params.idCategory],(err,row) =>{
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+};
+
 module.exports ={ GetCategories }
+module.exports ={ GetCategoriesById }
+module.exports ={ AddCategory }
+module.exports ={ DeleteCategory }
+module.exports ={ UpdateCategory }
--- src/Routes/Category.js
+++ src/Routes/Category.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../Controllers/CatController");
 
-router.get("/getcategories",userController.GetCategories);
+router.get("/api/getcategories",userController.GetCategories);
+router.get("/api/getcategories/:id",userController.GetCategoriesById);
+router.post("/api/addcategory",userController.AddCategory);
+router.delete("/api/deletecategory/:id",userController.DeleteCategory);
+router.put("/api/updatecategory/:id",userController.UpdateCategory);
 
 module.exports =router;
