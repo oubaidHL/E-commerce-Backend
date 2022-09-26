5374af90f2a651b8df621505786cf458a92c62fe
HLAIMI Oubaid <hlaymiabid@gmail.com>
2022-09-26 16:22:38 +0100

    first crud is done !

---
+++ src/Config/DatabaseConnection.js
@@ -0,0 +1,9 @@
+const mysql = require("mysql");
+require("dotenv").config();
+const db = mysql.createConnection({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+module.exports = db;
